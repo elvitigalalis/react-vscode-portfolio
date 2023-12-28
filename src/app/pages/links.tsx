873a20f078075d4aed038aea62da9b4c15d692dc
9016Lysol <elvitigalalis@gmail.com>
2023-12-28 02:05:37 -0500

    Commit React-VSCode Portfolio Implementation

---
+++ src/app/pages/links.tsx
@@ -0,0 +1,40 @@
+import {
+  FaMedium,
+  FaBlog,
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+} from "react-icons/fa";
+
+export const links = [
+  {
+    index: 0,
+    title: "Find me on Github",
+    href: "https://github.com/LEL16",
+    icon: <FaGithub />,
+  },
+  {
+    index: 1,
+    title: "Find me on LinkedIn",
+    href: "https://www.linkedin.com/in/lisul/",
+    icon: <FaLinkedin />,
+  },
+  {
+    index: 2,
+    title: "Contact me via email",
+    href: "mailto:elvitigalalis@gmail.com",
+    icon: <FaEnvelope />,
+  },
+  {
+    index: 3,
+    title: "Find me on Youtube",
+    href: "https://www.youtube.com/channel/UC3gbsCw3p7KLWinTEy7Z5Pg",
+    icon: <FaMedium />,
+  },
+  {
+    index: 4,
+    title: "Find me on Blog",
+    href: "https://noworneverev.github.io/blog/",
+    icon: <FaBlog />,
+  },
+];
